9d30278ed6a2550c03ed47a5593458b1dd0b7ef1
pp <substitutecat@foxmail.com>
2018-08-24 13:10:53 +0800

    Update bitmap.js

--- sub/canvas/bitmap.js
+++ sub/canvas/bitmap.js
@@ -3,17 +3,26 @@ import Sprite from './sprite';
 const ImageCache = {};
 
 export default class Bitmap extends Sprite {
-    constructor() {
+    /**
+     * @param circleFlag 是否需要裁剪为圆形
+     */
+    constructor(circleFlag) {
         super()
         this.originX = 0;
         this.originY = 0;
         this.image = null;
+        this.circleFlag = circleFlag
     }
     update(ctx) {
         if (!ctx) return;
         if (!this.image) return;
         ctx.save();
         this.transform(ctx);
+        if (this.circleFlag) { // 裁剪成圆形
+            ctx.beginPath()
+            ctx.arc(this.originX + this.width / 2 , this.originY + this.height / 2, this.width / 2, 0, 2 * Math.PI)
+            ctx.clip()
+        }
         ctx.drawImage(this.image, -this.width * this.originX, -this.height * this.originY, this.width, this.height);
         this.childUpdate(ctx);
         ctx.restore();
